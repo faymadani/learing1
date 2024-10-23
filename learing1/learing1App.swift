58b371cc95ed8cc8470a860789fd57e8d063d930
faymadani <faymadani99@gmail.com>
2024-10-23 09:43:45 +0300

    Initial Commit

---
+++ learing1/learing1App.swift
@@ -0,0 +1,17 @@
+//
+//  learing1App.swift
+//  learing1
+//
+//  Created by Fay Turad Madani on 20/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct learing1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
